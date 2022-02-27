a2fa54fba4270cd6bd87fb03d77f2f364330708b
Martin <mr.mmaurer+work@gmail.com>
2022-02-27 21:31:20 +0000

    Added some responsive styling

--- pages/simple-form-validation/index.tsx
+++ pages/simple-form-validation/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import type { NextPage } from "next";
 import { useForm, SubmitHandler } from "react-hook-form";
-import styles from "../../styles/Home.module.css";
+import styles from "./styles.module.css";
 
 import { Fields } from "./types";
 
@@ -41,38 +41,38 @@ const SFV: NextPage = () => {
           <label className={styles.label}>
             <span className="label-text">First Name</span>
             <input
+              {...register("firstName", { required: true })}
               type="text"
-              {...(register("firstName"), { required: true })}
               aria-invalid={Boolean(errors.firstName)}
               aria-describedby="field-1-errors"
             />
-            <span id="field-1-errors">
-              {Boolean(errors.firstName) && <span>Field has errors:</span>}
+            <span id="field-1-errors" className={styles.errors}>
+              {Boolean(errors.firstName) && <span className="decoration-red-100">Field has errors</span>}
               <pre>{errors.firstName?.message}</pre>
             </span>
           </label>
           <label className={styles.label}>
             <span className="label-text">Last Name</span>
             <input
+              {...register("lastName", { required: true })}
               type="text"
               aria-invalid={Boolean(errors.lastName)}
               aria-describedby="field-2-errors"
-              {...register("lastName", { required: true })}
             />
-            <span id="field-1-errors">
-              {Boolean(errors.lastName) && <span>Field has errors:</span>}
+            <span id="field-2-errors" className={styles.errors}>
+              {Boolean(errors.lastName) && <span className="decoration-red-100">Field has errors</span>}
               <pre>{errors.lastName?.message}</pre>
             </span>
           </label>
         </fieldset>
         <button
           type="submit"
-          className={`${styles.controls} border-solid border border-slate-600`}
+          className={`${styles.controls} p-2 border-solid border border-slate-600`}
         >
           Send
         </button>
-        <code className={styles.debug}>
-          <pre>{JSON.stringify(results)}</pre>
+        <code className={`${styles.debug} h-25 bg-slate-100 overflow-y-auto`}>
+          {Boolean(Object.keys(results).length) && <pre>{JSON.stringify(results, null, 2)}</pre>}
         </code>
       </form>
     </article>
