5153829e5fa262f8da7a6d60dde34ee40a8b5c88
Martin <mr.mmaurer+work@gmail.com>
2023-02-05 01:34:45 +0100

    Fixed stalled requests warning for api routes

--- pages/api/pizza/default-values.ts
+++ pages/api/pizza/default-values.ts
@@ -3,14 +3,20 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import type { FormValues } from '../../pizza/types';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<FormValues>) {
-  const defaultValues: FormValues = {
-    id: 'Username',
-    priceRangeClass: 'Standard',
-    selectedDough: 'American',
-    selectedToppings: ['Tomato'],
-  };
+  return new Promise(resolve => {
+    const defaultValues: FormValues = {
+      id: 'Username',
+      priceRangeClass: 'Standard',
+      selectedDough: 'American',
+      selectedToppings: ['Tomato'],
+    };
 
-  setTimeout(() => {
-    res.status(200).json(defaultValues);
-  }, Math.trunc(Math.random() * 10000));
+    setTimeout(() => {
+      res
+        .status(200)
+        .json(defaultValues);
+
+      resolve(res);
+    }, Math.trunc(Math.random() * 5000));
+  })
 }
