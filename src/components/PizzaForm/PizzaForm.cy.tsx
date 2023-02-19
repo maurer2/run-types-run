6b185c77d61b839d984f5d81c56a9ce90ccafd97
Martin <mr.mmaurer+work@gmail.com>
2023-02-19 01:44:02 +0000

    Migrated UncontrolledRadioCheckbox and disabled video recording

--- src/components/PizzaForm/PizzaForm.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PizzaForm from './PizzaForm'
-
-describe('<PizzaForm />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<PizzaForm />)
-  })
-})
--- src/components/UncontrolledRadioCheckbox/UncontrolledRadioCheckbox.cy.tsx
+++ src/components/UncontrolledRadioCheckbox/UncontrolledRadioCheckbox.cy.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useForm, FormProvider } from 'react-hook-form';
+
 import UncontrolledRadioCheckbox from './UncontrolledRadioCheckbox'
 
 describe('<UncontrolledRadioCheckbox />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<UncontrolledRadioCheckbox />)
+    // const formMethods = useForm({
+    //   defaultValues: {
+    //     name: ['one', two', 'three'],
+    //   },
+    //   mode: 'onChange',
+    // });
+
+    const getFieldStateMock = cy.stub().returns(true);
+    const registerMock = cy.stub();
+
+    cy.mount(
+      <FormProvider getFieldState={getFieldStateMock} register={registerMock}>
+        <UncontrolledRadioCheckbox type='radio' name="name" values={['one', 'two', 'three']} />
+      </FormProvider>
+    );
+
+    cy.findByText('Select name').should('exist');
+
+    cy.findByLabelText('one').should('exist');
+    cy.findByLabelText('two').should('exist');
+    cy.findByLabelText('three').should('exist');
+
+    cy.findAllByDisplayValue('one').should('exist');
+    cy.findAllByDisplayValue('two').should('exist');
+    cy.findAllByDisplayValue('three').should('exist');
   })
 })
