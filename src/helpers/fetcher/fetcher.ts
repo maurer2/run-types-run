79d49c0c0af15c2b64927982dce62b3b995e961f
Martin <mr.mmaurer+work@gmail.com>
2023-04-02 00:55:15 +0100

    Extracted fetcher and moved it into helper

---
+++ src/helpers/fetcher/fetcher.ts
@@ -0,0 +1,9 @@
+export const fetcher = async <T>(url: string): Promise<T> => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(response?.statusText || `Error fetching ${url}}`);
+  }
+
+  return response.json();
+};
