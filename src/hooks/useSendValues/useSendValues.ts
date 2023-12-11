a469d759cbba0aa2fd9d876e66ba27e6fb87ff5d
Martin <mr.mmaurer@gmail.com>
2023-12-11 17:12:27 +0000

    Simplified form value sending forgoing formData and url encoding

--- src/hooks/useSendValues/useSendValues.ts
+++ src/hooks/useSendValues/useSendValues.ts
@@ -1,19 +1,15 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { stringify } from 'qs';
 
 import type { FormValues } from '../../types/pizza';
 
 export const sendFormValues = async (url: string, payload: FormValues) => {
-  const payloadStringified = stringify(payload, {
-    arrayFormat: 'repeat',
-  });
-
-  const request = {
-    body: new URLSearchParams(payloadStringified),
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+  const request: RequestInit = {
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    },
     method: 'POST',
-  } satisfies RequestInit;
-
+  }
   const response = await fetch(url, request);
 
   if (!response.ok) {
