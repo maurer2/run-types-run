6b385c9cd54a4677c01d15c8d4cdeee95f70607e
Martin <mr.mmaurer@gmail.com>
2026-08-02 23:49:04 +0200

    Migrated to TS7

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typedRoutes: true,
+  experimental: {
+    useTypeScriptCli: true,
+  },
 };
 
 export default nextConfig;
