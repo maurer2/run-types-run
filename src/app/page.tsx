4768f288379ce12091d709272a64e1d3ab222ade
Martin <mr.mmaurer@gmail.com>
2026-08-02 23:43:35 +0200

    Added typed routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import Link from 'next/link';
+import type { Route } from 'next';
+
+const routes = [
+  { path: '/pizza?min-amount=2', label: 'Pizza' },
+  { path: '/deep-dish-pizza', label: 'Deep Dish Pizza' },
+  // { path: '/test', label: 'Test' },
+] satisfies { path: Route; label: string }[];
 
 export default function Home() {
   return (
     <div className="container max-w-4xl mx-auto px-8 py-8">
       <nav className="navbar navbar-center bg-neutral text-neutral-content">
-        <Link className="btn btn-ghost normal-case text-xl" href="/pizza?min-amount=2">
-          Pizza
-        </Link>
-        <Link className="btn btn-ghost normal-case text-xl" href="/deep-dish-pizza">
-          Deep Dish Pizza
-        </Link>
+        {routes.map(({ path, label }) => (
+          <Link key={path} className="btn btn-ghost normal-case text-xl" href={path}>
+            {label}
+          </Link>
+        ))}
       </nav>
     </div>
   );
